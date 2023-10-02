608713e87699e5769c8f2295e97b591ce89c6618
a.drobysheva <a.drobysheva@flash.ru>
2023-10-02 16:22:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿import pandas as pd
+
+# Создаем DataFrame
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+
+# Создаем пустой DataFrame с колонками, соответствующими уникальным значениям в столбце 'whoAmI'
+one_hot_data = pd.DataFrame(columns=data['whoAmI'].unique())
+
+# Заполняем one hot DataFrame
+for idx, value in data['whoAmI'].iteritems():
+    one_hot_data.loc[idx, value] = 1
+
+# Заменяем пропущенные значения на 0
+one_hot_data = one_hot_data.fillna(0)
+
+# Результат
+one_hot_data.head()
